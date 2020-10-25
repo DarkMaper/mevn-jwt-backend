9675ed6f6bca0046d0391f5e7d56729b20f06e74
= <=>
2020-10-25 18:23:01 +0100

    Added basic middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,24 @@
 import express, { Application } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
 
 import './database';
 
 export default class App {
 	private app: Application;
+
 	constructor() {
 		this.app = express();
+		this.middlewares();
+	}
+
+	private middlewares(): void {
+		this.app.use(cors());
+		this.app.use(helmet());
+		this.app.use(morgan('dev'));
+		this.app.use(express.urlencoded({ extended: false }));
+		this.app.use(express.json());
 	}
 
 	listen(): void {
