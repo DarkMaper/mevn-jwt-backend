015dae4b4ff8fc03239262dcbc70077dc0f29a42
= <=>
2020-10-26 20:57:59 +0100

    Implemented auth route

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,17 @@ import helmet from 'helmet';
 import cors from 'cors';
 
 import './database';
+import AuthRoutes from './routes/auth.routes';
 
 export default class App {
 	private app: Application;
+	private authRoutes: AuthRoutes;
 
 	constructor() {
 		this.app = express();
+		this.authRoutes = new AuthRoutes();
 		this.middlewares();
+		this.routes();
 	}
 
 	private middlewares(): void {
@@ -19,6 +23,10 @@ export default class App {
 		this.app.use(morgan('dev'));
 		this.app.use(express.urlencoded({ extended: false }));
 		this.app.use(express.json());
+	}
+
+	private routes() {
+		this.app.use('/api/auth', this.authRoutes.router);
 	}
 
 	listen(): void {
