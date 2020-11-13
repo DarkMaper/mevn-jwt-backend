6caad29c32ba8deba3cca00414339b51483f93af
DarkMaper <darkmaper@gmail.com>
2020-11-13 08:58:32 +0100

    Added new routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import AuthController from '../controllers/auth.controllers';
+import { isAuthenticated } from '../middlewares/authorization';
 
 export default class AuthRoutes {
 	public router: Router;
@@ -12,7 +13,11 @@ export default class AuthRoutes {
 	}
 
 	private routes = async () => {
-		console.log('Routes called');
 		this.router.post('/login', this.authController.loginUser);
+		this.router.post('/logout', this.authController.logout);
+		this.router.get('/refresh', this.authController.refreshToken);
+		this.router.get('/test', isAuthenticated, (req, res) => {
+			res.json({ message: '' });
+		});
 	};
 }
