9f4b6d2e6664e3f0d9d3620fa8af88ebc93d5f64
DarkMaper <darkmaper@gmail.com>
2020-11-13 09:03:48 +0100

    Added all configurations

--- src/server.ts
+++ src/server.ts
@@ -2,27 +2,43 @@ import express, { Application } from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import './database';
+import './config/passport';
+
 import AuthRoutes from './routes/auth.routes';
+import { createRoles, createAdminUser } from './lib/initialConfig';
 
 export default class App {
 	private app: Application;
 	private authRoutes: AuthRoutes;
+	private cookieSecret: string;
+	private port: number;
 
-	constructor() {
+	constructor(port = 3000) {
 		this.app = express();
+		this.port = parseInt(process.env.PORT || `${port}`);
 		this.authRoutes = new AuthRoutes();
+		createRoles();
+		createAdminUser();
+		this.cookieSecret = process.env.COOKIE_SECRET || 'replacethis';
 		this.middlewares();
 		this.routes();
 	}
 
 	private middlewares(): void {
-		this.app.use(cors());
-		this.app.use(helmet());
 		this.app.use(morgan('dev'));
+		this.app.use(
+			cors({
+				origin: ['http://localhost:8080'],
+				credentials: true,
+			}),
+		);
+		this.app.use(helmet());
 		this.app.use(express.urlencoded({ extended: false }));
 		this.app.use(express.json());
+		this.app.use(cookieParser(this.cookieSecret));
 	}
 
 	private routes() {
@@ -30,7 +46,7 @@ export default class App {
 	}
 
 	listen(): void {
-		this.app.listen(3000);
-		console.log('Server listening in port 3000');
+		this.app.listen(this.port);
+		console.log('Server listening in port', this.port);
 	}
 }
